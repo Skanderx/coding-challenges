0c5b2a1cd082d754f0a531c3e69e3208dc54389b
Skander <ibenamor@ats-digital.com>
2025-01-08 21:12:50 +0100

    feat: step 1

--- challenge 2/json_parser.go
+++ challenge 2/json_parser.go
@@ -1,8 +1,68 @@
 package parser
 
-import "errors"
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+// give the index of the first non-space character, from the start index
+func skipSpaces(json string, start int) (int, error) {
+	i := start
+	// if !unicode.IsSpace(rune(json[i])) {
+	// 	return i, nil
+	// }
+	for {
+		if i >= len(json) {
+			return -1, errors.New("invalid json")
+		}
+		if !unicode.IsSpace(rune(json[i])) {
+			return i, nil
+		}
+		i++
+	}
+}
+func findString(json string, start int) (int, string, error) {
+	i := start
+	if json[i] != '"' {
+		return -1, "", errors.New("invalid json expected \" but got " + string(json[i]))
+	}
+	i++
+	val := ""
+	for {
+		if i >= len(json) {
+			return -1, "", errors.New("invalid json")
+		}
+		if json[i] == '"' {
+			break
+		}
+		val += string(json[i])
+		i++
+	}
+	return i + 1, val, nil
+}
+func findValue(json string, start int) (int, string, error) {
+	i := start
+	if json[i] != '"' {
+		return -1, "", errors.New("invalid json expected \" but got " + string(json[i]))
+	}
+	i++
+	val := ""
+	for {
+		if i >= len(json) {
+			return -1, "", errors.New("invalid json")
+		}
+		if json[i] == '"' {
+			break
+		}
+		val += string(json[i])
+		i++
+	}
+	return i + 1, val, nil
+}
 
 func Parse(json string) (map[string]interface{}, error) {
+	json = strings.TrimSpace(json)
 	if len(json) == 0 {
 		return nil, errors.New("empty file")
 	}
@@ -11,5 +71,58 @@ func Parse(json string) (map[string]interface{}, error) {
 		return nil, errors.New("invalid json")
 	}
 
-	return nil, nil
+	result := make(map[string]interface{})
+	jsonLength := len(json)
+	i := 0
+	for i < jsonLength {
+
+		// open object
+		if json[i] == '{' {
+			i++
+		}
+		var err error
+		i, err = skipSpaces(json, i)
+		if err != nil {
+			return nil, err
+		}
+		fieldName := ""
+		i, fieldName, err = findString(json, i)
+		if err != nil {
+			return nil, err
+		}
+		// find :
+		i, err = skipSpaces(json, i)
+		if err != nil {
+			return nil, err
+		}
+		if json[i] != ':' {
+			return nil, errors.New("invalid json expected : but got " + string(json[i]))
+		}
+		i++
+		// field value
+		i, err = skipSpaces(json, i)
+		if err != nil {
+			return nil, err
+		}
+		fieldValue := ""
+		i, fieldValue, err = findValue(json, i) // TODO: test field name isn't empty
+		if err != nil {
+			return nil, err
+		}
+		result[fieldName] = fieldValue
+		// either , or }
+		i, err = skipSpaces(json, i)
+		if err != nil {
+			return nil, err
+		}
+		// either , or }
+		if json[i] != ',' {
+			if json[i] != '}' {
+				return nil, err
+			}
+		}
+		i++
+	}
+
+	return result, nil
 }
--- challenge 2/json_parser_test.go
+++ challenge 2/json_parser_test.go
@@ -35,3 +35,58 @@ func TestParseStep1(t *testing.T) {
 		t.Log("empty is not a valid json")
 	})
 }
+
+func TestParseStep2(t *testing.T) {
+
+	t.Run("valid", func(t *testing.T) {
+		jsonString, err := os.ReadFile("tests/step2/valid.json")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		_, err = parser.Parse(string(jsonString))
+		if err != nil {
+			t.Error(err)
+
+		}
+		t.Log(string(jsonString) + " is a valid json")
+	})
+	t.Run("valid 2", func(t *testing.T) {
+		jsonString, err := os.ReadFile("tests/step2/valid2.json")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		_, err = parser.Parse(string(jsonString))
+		if err != nil {
+			t.Error(err)
+
+		}
+		t.Log(string(jsonString) + " is a valid json")
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		jsonString, err := os.ReadFile("tests/step2/invalid.json")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		_, err = parser.Parse(string(jsonString))
+		if err == nil {
+			t.Error("invalid json is not detected")
+		}
+		t.Log(string(jsonString) + " is not a valid json")
+	})
+	t.Run("invalid 2", func(t *testing.T) {
+		jsonString, err := os.ReadFile("tests/step2/invalid2.json")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		_, err = parser.Parse(string(jsonString))
+		if err == nil {
+			t.Error("invalid json is not detected")
+		}
+		t.Log(string(jsonString) + " is not a valid json")
+	})
+}
