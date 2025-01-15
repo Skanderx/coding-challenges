05e3f43dc5e2ac0b440f4b7678829441712d4bbc
Skander <ibenamor@ats-digital.com>
2025-01-15 23:13:57 +0100

    feat: step 2 Huffman tree

--- challenge 3/compressor/compressor.go
+++ challenge 3/compressor/compressor.go
@@ -1,9 +1,12 @@
 package compressor
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"golang.org/x/exp/slices"
 )
 
 func countFrequency(data []byte) map[rune]int {
@@ -14,6 +17,54 @@ func countFrequency(data []byte) map[rune]int {
 	return freq
 }
 
+type TreeNode struct {
+	Freq  int       `json:"freq"`
+	Rune  rune      `json:"char"`
+	Left  *TreeNode `json:"left"`
+	Right *TreeNode `json:"right"`
+}
+
+func (t *TreeNode) String() string {
+	result, err := json.Marshal(t)
+	if err != nil {
+		fmt.Println("cannot convert tree", err)
+		return ""
+	}
+	return string(result)
+}
+func makeTree(freq map[rune]int) *TreeNode {
+	result := make([]*TreeNode, len(freq))
+	i := 0
+	for k, v := range freq {
+		result[i] = &TreeNode{Freq: v, Rune: k}
+		i++
+	}
+	slices.SortFunc(result, func(a, b *TreeNode) int {
+		return a.Freq - b.Freq
+	})
+	for {
+		if len(result) == 1 {
+			break
+		}
+		min1 := result[0]
+		min2 := result[1]
+		result = result[2:]
+		joinedNode := &TreeNode{
+			Freq:  min1.Freq + min2.Freq,
+			Left:  min1,
+			Right: min2,
+		}
+		insertionIndex := slices.IndexFunc(result, func(a *TreeNode) bool {
+			return a.Freq > joinedNode.Freq
+		})
+		if insertionIndex == -1 {
+			insertionIndex = len(result)
+		}
+		result = slices.Insert(result, insertionIndex, joinedNode)
+	}
+
+	return result[0]
+}
 func Compress() (string, error) {
 
 	// Read file
@@ -25,7 +76,9 @@ func Compress() (string, error) {
 
 	}
 	// Count frequency of characters
-	countFrequency(data)
-
+	ferequencies := countFrequency(data)
+	// Make Huffman coding tree
+	root := makeTree(ferequencies)
+	fmt.Println(root)
 	return "", nil
 }
--- challenge 3/compressor/compressor_test.go
+++ challenge 3/compressor/compressor_test.go
@@ -30,3 +30,96 @@ func TestCountFrequency(t *testing.T) {
 		}
 	})
 }
+
+func TestMakeTree(t *testing.T) {
+	data, err := os.ReadFile(TESTFILEPATH)
+	if err != nil {
+		t.Errorf("error reading file: %v\n", err)
+		return
+	}
+	freq := countFrequency(data)
+	root := makeTree(freq)
+	t.Run("root node has correct frequency", func(t *testing.T) {
+		freqSum := 0
+		for _, v := range freq {
+			freqSum += v
+		}
+		if root.Freq != freqSum {
+			t.Errorf("expected sum of frequencies to be %d, got %d\n", freqSum, root.Freq)
+		} else {
+			fmt.Println("root node has correct frequency")
+		}
+	})
+	t.Run("tree on example", func(t *testing.T) {
+		freq := map[rune]int{
+			'C': 32,
+			'D': 42,
+			'E': 120,
+			'K': 7,
+			'L': 42,
+			'M': 24,
+			'U': 37,
+			'Z': 2,
+		}
+		tree := makeTree(freq)
+		reference := &TreeNode{
+			Freq: 306, Rune: 0,
+			Left: &TreeNode{
+				Freq: 120, Rune: 'E',
+				Left: nil, Right: nil,
+			},
+			Right: &TreeNode{
+				Freq: 186, Rune: 0,
+				Left: &TreeNode{
+					Freq: 79, Rune: 0,
+					Left: &TreeNode{
+						Freq: 37, Rune: 'U',
+						Left: nil, Right: nil,
+					},
+					Right: &TreeNode{
+						Freq: 42, Rune: 'D',
+						Left: nil, Right: nil,
+					},
+				},
+				Right: &TreeNode{
+					Freq: 107, Rune: 0,
+					Left: &TreeNode{
+						Freq: 42, Rune: 'L',
+						Left: nil, Right: nil,
+					},
+					Right: &TreeNode{
+						Freq: 65, Rune: 0,
+						Left: &TreeNode{
+							Freq: 32, Rune: 'C',
+							Left: nil, Right: nil,
+						},
+						Right: &TreeNode{
+							Freq: 33, Rune: 0,
+							Left: &TreeNode{
+								Freq: 9, Rune: 0,
+								Left: &TreeNode{
+									Freq: 2, Rune: 'Z',
+									Left: nil, Right: nil,
+								},
+								Right: &TreeNode{
+									Freq: 7, Rune: 'K',
+									Left: nil, Right: nil,
+								},
+							},
+							Right: &TreeNode{
+								Freq: 24, Rune: 'M',
+								Left: nil, Right: nil,
+							},
+						},
+					},
+				},
+			},
+		}
+		if tree.String() != reference.String() {
+			t.Errorf("expected tree to be %v, got %v\n", reference, tree)
+			fmt.Println("tree on example correct")
+		} else {
+			fmt.Println("tree on example correct")
+		}
+	})
+}
