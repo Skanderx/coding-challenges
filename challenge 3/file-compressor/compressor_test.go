fe48a8823c0ef46f8ef517d0ede02c8f095b4db4
Skander <ibenamor@ats-digital.com>
2025-01-31 22:35:06 +0100

    progress on compressor

--- challenge 3/file-compressor/compressor_test.go
+++ challenge 3/file-compressor/compressor_test.go
@@ -12,7 +12,7 @@ const TESTFILEPATH = "../../challenge 3/test.txt"
 func TestCountFrequency(t *testing.T) {
 	data, err := os.ReadFile(TESTFILEPATH)
 	if err != nil {
-		t.Errorf("error reading file: %v\n", err)
+		t.Skipf("error reading file: %v\n", err)
 		return
 	}
 	freq := countFrequency(data)
@@ -32,141 +32,150 @@ func TestCountFrequency(t *testing.T) {
 	})
 }
 
-var freqExample = map[byte]int{
-	'C': 32,
-	'D': 42,
-	'E': 120,
-	'K': 7,
-	'L': 42,
-	'M': 24,
-	'U': 37,
-	'Z': 2,
-}
-var referenceTree = &TreeNode{
-	Freq: 306, Char: 0,
-	Left: &TreeNode{
-		Freq: 120, Char: 'E',
-		Left: nil, Right: nil,
-	},
-	Right: &TreeNode{
-		Freq: 186, Char: 0,
-		Left: &TreeNode{
-			Freq: 79, Char: 0,
-			Left: &TreeNode{
-				Freq: 37, Char: 'U',
-				Left: nil, Right: nil,
-			},
-			Right: &TreeNode{
-				Freq: 42, Char: 'D',
-				Left: nil, Right: nil,
-			},
+func TestEncoderDecoder(t *testing.T) {
+	tests := []struct {
+		freq               map[byte]int
+		referenceTree      *TreeNode
+		referencePrefixMap map[byte]byte
+	}{{
+		freq: map[byte]int{
+			'C': 32,
+			'D': 42,
+			'E': 120,
+			'K': 7,
+			'L': 42,
+			'M': 24,
+			'U': 37,
+			'Z': 2,
 		},
-		Right: &TreeNode{
-			Freq: 107, Char: 0,
+		referenceTree: &TreeNode{
+			Freq: 306, Char: 0,
 			Left: &TreeNode{
-				Freq: 42, Char: 'L',
+				Freq: 120, Char: 'E',
 				Left: nil, Right: nil,
 			},
 			Right: &TreeNode{
-				Freq: 65, Char: 0,
+				Freq: 186, Char: 0,
 				Left: &TreeNode{
-					Freq: 32, Char: 'C',
-					Left: nil, Right: nil,
-				},
-				Right: &TreeNode{
-					Freq: 33, Char: 0,
+					Freq: 79, Char: 0,
 					Left: &TreeNode{
-						Freq: 9, Char: 0,
-						Left: &TreeNode{
-							Freq: 2, Char: 'Z',
-							Left: nil, Right: nil,
-						},
-						Right: &TreeNode{
-							Freq: 7, Char: 'K',
-							Left: nil, Right: nil,
-						},
+						Freq: 37, Char: 'U',
+						Left: nil, Right: nil,
 					},
 					Right: &TreeNode{
-						Freq: 24, Char: 'M',
+						Freq: 42, Char: 'D',
 						Left: nil, Right: nil,
 					},
 				},
+				Right: &TreeNode{
+					Freq: 107, Char: 0,
+					Left: &TreeNode{
+						Freq: 42, Char: 'L',
+						Left: nil, Right: nil,
+					},
+					Right: &TreeNode{
+						Freq: 65, Char: 0,
+						Left: &TreeNode{
+							Freq: 32, Char: 'C',
+							Left: nil, Right: nil,
+						},
+						Right: &TreeNode{
+							Freq: 33, Char: 0,
+							Left: &TreeNode{
+								Freq: 9, Char: 0,
+								Left: &TreeNode{
+									Freq: 2, Char: 'Z',
+									Left: nil, Right: nil,
+								},
+								Right: &TreeNode{
+									Freq: 7, Char: 'K',
+									Left: nil, Right: nil,
+								},
+							},
+							Right: &TreeNode{
+								Freq: 24, Char: 'M',
+								Left: nil, Right: nil,
+							},
+						},
+					},
+				},
 			},
 		},
-	},
-}
-var referencePrefixMap = map[byte]byte{
-	'C': 0b1110,
-	'D': 0b101,
-	'E': 0b0,
-	'K': 0b111101,
-	'L': 0b110,
-	'M': 0b11111,
-	'U': 0b100,
-	'Z': 0b111100,
-}
+		referencePrefixMap: map[byte]byte{
+			'C': 0b1110,
+			'D': 0b101,
+			'E': 0b0, // Starting with 0 is a problem
+			'K': 0b111101,
+			'L': 0b110,
+			'M': 0b11111,
+			'U': 0b100,
+			'Z': 0b111100,
+		},
+	}}
+	for _, tt := range tests {
+		t.Run("Make tree", func(t *testing.T) {
+			data, err := os.ReadFile(TESTFILEPATH)
+			if err != nil {
+				t.Errorf("error reading file: %v\n", err)
+				return
+			}
+			freq := countFrequency(data)
+			root := makeTree(freq)
+			t.Run("root node has correct frequency", func(t *testing.T) {
+				freqSum := 0
+				for _, v := range freq {
+					freqSum += v
+				}
+				if root.Freq != freqSum {
+					t.Errorf("expected sum of frequencies to be %d, got %d\n", freqSum, root.Freq)
+				} else {
+					fmt.Println("root node has correct frequency")
+				}
+			})
+			t.Run("tree on example", func(t *testing.T) {
 
-func TestMakeTree(t *testing.T) {
-	data, err := os.ReadFile(TESTFILEPATH)
-	if err != nil {
-		t.Errorf("error reading file: %v\n", err)
-		return
+				tree := makeTree(tt.freq)
+				if tree.String() != tt.referenceTree.String() {
+					t.Errorf("expected tree to be %v, got %v\n", tt.referenceTree, tree)
+					fmt.Println("tree on example correct")
+				} else {
+					fmt.Println("tree on example correct")
+				}
+			})
+		})
+
+		t.Run("Make prefix map", func(t *testing.T) {
+			t.Run("prefix map on example", func(t *testing.T) {
+
+				prefixMap := make(map[byte]byte, len(tt.freq))
+
+				generatePrefixCodes(prefixMap, tt.referenceTree, 0)
+
+				if !maps.Equal(prefixMap, tt.referencePrefixMap) {
+					t.Errorf("PrefixMap() returned %v, want %v", prefixMap, tt.referencePrefixMap)
+				} else {
+					fmt.Println("PrefixMap() correct")
+				}
+			})
+		})
+
+		t.Run("File Header", func(t *testing.T) {
+			t.Run("file header on example", func(t *testing.T) {
+				prefixMap := make(map[byte]byte, len(tt.freq))
+				generatePrefixCodes(prefixMap, tt.referenceTree, 0)
+				header := fileHeader(prefixMap)
+
+				_, prefixMapDecoded, err := decodeHeader(header)
+				if err != nil {
+					t.Errorf("error decoding header: %v\n", err)
+					return
+				}
+				if !maps.Equal(prefixMap, prefixMapDecoded) {
+					t.Errorf("PrefixMap() returned %v, want %v", prefixMap, prefixMapDecoded)
+				} else {
+					fmt.Println("PrefixMap() correct")
+				}
+			})
+		})
 	}
-	freq := countFrequency(data)
-	root := makeTree(freq)
-	t.Run("root node has correct frequency", func(t *testing.T) {
-		freqSum := 0
-		for _, v := range freq {
-			freqSum += v
-		}
-		if root.Freq != freqSum {
-			t.Errorf("expected sum of frequencies to be %d, got %d\n", freqSum, root.Freq)
-		} else {
-			fmt.Println("root node has correct frequency")
-		}
-	})
-	t.Run("tree on example", func(t *testing.T) {
-
-		tree := makeTree(freqExample)
-		if tree.String() != referenceTree.String() {
-			t.Errorf("expected tree to be %v, got %v\n", referenceTree, tree)
-			fmt.Println("tree on example correct")
-		} else {
-			fmt.Println("tree on example correct")
-		}
-	})
-}
-
-func TestPrefixMap(t *testing.T) {
-	t.Run("prefix map on example", func(t *testing.T) {
-
-		prefixMap := make(map[byte]byte, len(freqExample))
-
-		generatePrefixCodes(prefixMap, referenceTree, 0)
-
-		if !maps.Equal(prefixMap, referencePrefixMap) {
-			t.Errorf("PrefixMap() returned %v, want %v", prefixMap, referencePrefixMap)
-		} else {
-			fmt.Println("PrefixMap() correct")
-		}
-	})
-}
-
-func TestFileHeader(t *testing.T) {
-	t.Run("file header on example", func(t *testing.T) {
-		prefixMap := make(map[byte]byte, len(freqExample))
-		generatePrefixCodes(prefixMap, referenceTree, 0)
-		header := fileHeader(prefixMap)
-
-		_, prefixMapDecoded, err := decodeHeader(header)
-		if err != nil {
-			t.Errorf("error decoding header: %v\n", err)
-			return
-		}
-		if !maps.Equal(prefixMap, prefixMapDecoded) {
-			t.Errorf("PrefixMap() returned %v, want %v", prefixMap, prefixMapDecoded)
-		} else {
-			fmt.Println("PrefixMap() correct")
-		}
-	})
 }
--- challenge 3/file-compressor/encoder.go
+++ challenge 3/file-compressor/encoder.go
@@ -72,35 +72,12 @@ func Compress(fileName string) error {
 
 	w := bufio.NewWriter(f)
 	// Writing 8 bits at a time; but having space for at least two
-	var buffer uint16
+	var buffer struct {
+		maxBitIndex int
+		buffer      uint16
+	}
 
 	for _, r := range data {
-
-		// buffer	= xxxxxxxx_1yyyyyyy
-		// if buffer reached this value, we at least have one byte we can write
-		if buffer > 128 {
-			maxBitIndex := 16
-			for maxBitIndex > 8 {
-				if buffer < Pow2_16[maxBitIndex-1] {
-					maxBitIndex--
-				} else {
-					break
-				}
-			}
-			// Write 8 bit slices at a time
-			// buffer	= 000000yy_yyyyyyzz ;  maxBitIndex = 10
-			// slice	= 000000yy_yyyyyyzz >> ( maxBitIndex - 7 + 1)
-			// => slice	= 000000yy_yyyyyyzz >> 2
-			// => slice	= 00000000Myyyyyyy : 8 bits
-			slice := buffer
-			slice >>= (maxBitIndex - 7 + 1)
-			// what should stay of buffer = 000000_000000zz = buffer % math.Pow(2, float64(maxBitIndex-7))
-			buffer = buffer % Pow2_16[(maxBitIndex-7+1)]
-			err = w.WriteByte(byte(slice))
-			if err != nil {
-				return fmt.Errorf("error writing to file: %w", err)
-			}
-		}
 		// Adding prefix_Code in queue to be written
 		// Solution without google:
 		value := prefixMap[r]
@@ -110,25 +87,52 @@ func Compress(fileName string) error {
 		// => buffer	= 00000aaa | xxxxx_yyyyyyyy000
 		// => buffer	= xxxxx_yyyyyyyyaaa
 		case value >= 128: // 8 bit value
-			buffer = uint16(value) | buffer<<8
+			buffer.buffer = uint16(value) | buffer.buffer<<8
+			buffer.maxBitIndex += 8
 		case value >= 64: // 7 bit value
-			buffer = uint16(value) | buffer<<7
+			buffer.buffer = uint16(value) | buffer.buffer<<7
+			buffer.maxBitIndex += 7
 		case value >= 32: // 6 bit value
-			buffer = uint16(value) | buffer<<6
+			buffer.buffer = uint16(value) | buffer.buffer<<6
+			buffer.maxBitIndex += 6
 		case value >= 16:
-			buffer = uint16(value) | buffer<<5
+			buffer.buffer = uint16(value) | buffer.buffer<<5
+			buffer.maxBitIndex += 5
 		case value >= 8:
-			buffer = uint16(value) | buffer<<4
+			buffer.buffer = uint16(value) | buffer.buffer<<4
+			buffer.maxBitIndex += 4
 		case value >= 4:
-			buffer = uint16(value) | buffer<<3
+			buffer.buffer = uint16(value) | buffer.buffer<<3
+			buffer.maxBitIndex += 3
 		case value >= 2:
-			buffer = uint16(value) | buffer<<2
+			buffer.buffer = uint16(value) | buffer.buffer<<2
+			buffer.maxBitIndex += 2
 		default:
-			buffer = uint16(value) | buffer<<1
+			buffer.buffer = uint16(value) | buffer.buffer<<1
+			buffer.maxBitIndex += 1
+		}
+
+		// buffer	= xxxxxxxx_1yyyyyyy
+		// if buffer reached this value, we at least have one byte we can write
+		if buffer.maxBitIndex > 8 {
+
+			// Write 8 bit slices at a time
+			// buffer	= 000000yy_yyyyyyzz ;  maxBitIndex = 10
+			// slice	= 000000yy_yyyyyyzz >> ( maxBitIndex - 7 + 1)
+			// => slice	= 000000yy_yyyyyyzz >> 2
+			// => slice	= 00000000Myyyyyyy : 8 bits
+			slice := buffer.buffer >> (buffer.maxBitIndex - 7 + 1)
+			err = w.WriteByte(byte(slice))
+			if err != nil {
+				return fmt.Errorf("error writing to file: %w", err)
+			}
+			// what should stay in buffer = 000000_000000zz = buffer % math.Pow(2, float64(maxBitIndex-7))
+			buffer.buffer = buffer.buffer % Pow2_16[(buffer.maxBitIndex-7+1)]
+			buffer.maxBitIndex -= 8
 		}
 	}
-	if buffer > 0 {
-		slice := byte(buffer >> 8)
+	if buffer.maxBitIndex > 0 {
+		slice := byte(buffer.buffer >> (buffer.maxBitIndex - 7 + 1))
 		err = w.WriteByte(slice)
 		if err != nil {
 			return fmt.Errorf("error writing to file: %w", err)
