6593d65ff5fdb713a84b4c472e8217df60478607
Skander <ibenamor@ats-digital.com>
2025-02-06 19:19:59 +0100

    feat: challenge 4 step 1

--- challenge 4/cut/cut.go
+++ challenge 4/cut/cut.go
@@ -1,1 +1,11 @@
 package cut
+
+import "strings"
+
+func Fields(dataIn []byte, index int) string {
+	out := ""
+	for _, line := range strings.Split(string(dataIn), "\n") {
+		out += strings.Split(line, "\t")[index] + "\n"
+	}
+	return out
+}
--- challenge 4/main.go
+++ challenge 4/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"main/cut"
+
+	"os"
+	"path/filepath"
+)
+
+var flags = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+var fields int // []int
+
+func init() {
+	flags.IntVar(&fields, "f", 1, "Fields to cut")
+}
 
 func main() {
-	fmt.Println("cut.Words()")
+	flags.Parse(os.Args[1:])
+
+	fileName := filepath.Clean(os.Args[len(os.Args)-1])
+
+	dataIn, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if fields < 1 {
+		fmt.Println("Fields must be greater than 0")
+		return
+	}
+	fmt.Println(cut.Fields(dataIn, fields-1))
 }
