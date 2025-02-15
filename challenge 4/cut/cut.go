a31a1ea8db79b6281da83c82b037760fe823801c
Skander <ibenamor@ats-digital.com>
2025-02-15 18:17:00 +0100

    feat: step 3

--- challenge 4/cut/cut.go
+++ challenge 4/cut/cut.go
@@ -2,10 +2,17 @@ package cut
 
 import "strings"
 
-func Fields(dataIn []byte, index int, delimiter rune) string {
+func Fields(dataIn []byte, index []int, delimiter rune) string {
 	out := ""
 	for _, line := range strings.Split(string(dataIn), "\n") {
-		out += strings.Split(line, string(delimiter))[index] + "\n"
+		columns := strings.Split(line, string(delimiter))
+		for _, index := range index {
+			if index >= len(columns) {
+				continue
+			}
+			out += columns[index] + string(delimiter)
+		}
+		out += "\n"
 	}
 	return out
 }
--- challenge 4/main.go
+++ challenge 4/main.go
@@ -4,18 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"main/cut"
+	"strconv"
+	"strings"
 
 	"os"
 	"path/filepath"
 )
 
 var flags = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-var fields int // []int
+var fields string
 var delimiter string
 
 func init() {
-	flags.IntVar(&fields, "f", 1, "Fields to cut")
+	flags.StringVar(&fields, "f", "1", "Fields to cut")
 	flags.StringVar(&delimiter, "d", "\t", "Delimiter")
+}
+func splitFields(fields string) []string {
+	if unquotedFields := strings.Trim(fields, "\""); unquotedFields != fields {
+		return strings.Split(unquotedFields, " ")
+	}
+	return strings.Split(fields, ",")
 }
 
 func main() {
@@ -28,10 +36,20 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	if fields < 1 {
+	if len(fields) == 0 {
 		fmt.Println("Fields must be greater than 0")
 		return
 	}
+	fieldsString := splitFields(fields)
+	fieldIndexes := make([]int, len(fieldsString))
+	for i, field := range fieldsString {
+		fieldIndex, err := strconv.Atoi(field)
+		if err != nil {
+			fmt.Printf("field %s is not a number\n : %s", field, err)
+			return
+		}
+		fieldIndexes[i] = fieldIndex - 1
+	}
 
-	fmt.Println(cut.Fields(dataIn, fields-1, []rune(delimiter)[0]))
+	fmt.Println(cut.Fields(dataIn, fieldIndexes, []rune(delimiter)[0]))
 }
